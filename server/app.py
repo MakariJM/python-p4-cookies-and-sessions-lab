c99c0c2bddb99167754485e1d00b3e2e2af179f4
JuliusMakari <muragejmakari@gmail.com>
2024-04-13 04:57:00 +0300

    resubmition

--- server/app.py
+++ server/app.py
@@ -5,18 +5,19 @@ from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
+def configure_app():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+configure_app()
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    return jsonify({'message': 'Successfully cleared session data'}), 200
 
 @app.route('/articles')
 def index_articles():
@@ -45,4 +46,5 @@ def show_article(id):
 
 if __name__ == '__main__':
     app.run(port=5555)
+
 
